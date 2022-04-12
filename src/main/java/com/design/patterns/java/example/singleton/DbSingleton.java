8e495af8f4d428482ed73c6a5e26b646eb3860f8
NicolasMRTNS <nicolas.martins@hotmail.fr>
2022-04-12 15:28:13 +0200

    Builder pattern

--- src/main/java/com/design/patterns/java/example/singleton/DbSingleton.java
+++ src/main/java/com/design/patterns/java/example/singleton/DbSingleton.java
@@ -1,13 +1,14 @@
 package com.design.patterns.java.example.singleton;
 
 public class DbSingleton {
+	
 	public static volatile DbSingleton instance = null;
 	
 	private DbSingleton() {
 		if (instance != null) {
 			throw new RuntimeException("Use getInstance() to create");
 		}
-	};
+	}
 	
 	public static DbSingleton getInstance() {
 		if (instance == null) {
